6ba3abd745995958ba9095936003a92c275c7873
Nikola Garabandic <kantagara@gmail.com>
2024-07-31 16:54:43 +0200

    10 - Rotating the player and placing the props

--- Assets/Photon/Quantum/Runtime/QuantumDebugInput.cs
+++ Assets/Photon/Quantum/Runtime/QuantumDebugInput.cs
@@ -6,10 +6,11 @@ namespace Quantum {
   /// A Unity script that creates empty input for any Quantum game.
   /// </summary>
   public class QuantumDebugInput : MonoBehaviour {
-
+    private Vector3 _mouseHitPosition;
     private void OnEnable() {
       QuantumCallback.Subscribe(this, (CallbackPollInput callback) => PollInput(callback));
     }
+    
 
     /// <summary>
     /// Set an empty input when polled by the simulation.
@@ -17,6 +18,11 @@ namespace Quantum {
     /// <param name="callback"></param>
     public void PollInput(CallbackPollInput callback) {
       Quantum.Input i = new Quantum.Input();
+
+      var ray = Camera.main.ScreenPointToRay(UnityEngine.Input.mousePosition);
+      if (Physics.Raycast(ray, out var hit, 100, 1 << UnityEngine.LayerMask.NameToLayer("Ground")))
+        _mouseHitPosition = hit.point;
+      i.MousePosition = _mouseHitPosition.ToFPVector3().XZ;
       i.Direction = new FPVector2(UnityEngine.Input.GetAxis("Horizontal").ToFP(), UnityEngine.Input.GetAxis("Vertical").ToFP());
       callback.SetInput(i, DeterministicInputFlags.Repeatable);
     }
