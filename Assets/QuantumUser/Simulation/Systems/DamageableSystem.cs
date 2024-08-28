d1e45aa4a553600dcf13b3696972ee135acdf8c8
Nikola Garabandic <kantagara@gmail.com>
2024-08-28 16:54:09 +0200

    16 - Data-Driven Polymorphism

--- Assets/QuantumUser/Simulation/Systems/DamageableSystem.cs
+++ Assets/QuantumUser/Simulation/Systems/DamageableSystem.cs
@@ -14,13 +14,11 @@ namespace Quantum {
       component->Health = damageableData.MaxHealth;
     }
 
-    public void DamageableHit(Frame f, EntityRef damageableEntity, FP damage, Damageable* damageable)
+
+    public void DamageableHit(Frame f, EntityRef victim, EntityRef hitter, FP damage, Damageable* damageable)
     {
-      damageable->Health -= damage;
-      if (damageable->Health < 0)
-      {
-        f.Destroy(damageableEntity);
-      }
+      var damageableBase = f.FindAsset<DamageableBase>(damageable->DamageableData);
+      damageableBase.DamageableHit(f, victim, hitter, damage, damageable);
     }
   }
 }
