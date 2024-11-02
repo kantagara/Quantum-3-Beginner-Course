df53fd8934dccb99932c1731306b2078d8e91332
Nikola Garabandic <kantagara@gmail.com>
2024-11-02 22:24:04 +0100

    31 - GameManager & Game State

--- Assets/QuantumUser/View/CountdownUI.cs
+++ Assets/QuantumUser/View/CountdownUI.cs
@@ -11,6 +11,25 @@ public class CountdownUI : QuantumSceneViewComponent
     [SerializeField] private TMP_Text timeRemainingText;
     [SerializeField] private Image timeProgressImage;
 
+    public override void OnActivate(Frame frame)
+    {
+        QuantumEvent.Subscribe<EventGameOver>(this, GameOver);
+    }
+
+    private void GameOver(EventGameOver callback)
+    {
+        var f = callback.Game.Frames.Predicted;
+        var playerRef = f.Get<PlayerLink>(callback.Winner).Player;
+        var playerData = f.GetPlayerData(playerRef);
+        Debug.Log("Winner is " + playerData.PlayerNickname);
+
+    }
+
+    public override void OnDeactivate()
+    {
+        QuantumEvent.UnsubscribeListener(this);
+    }
+
     public override void OnUpdateView()
     {
         var f = PredictedFrame;
